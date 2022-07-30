bfff1f53a46b641597a24c0ee02edc25d57cda39
magdalena <@gmail.com>
2022-07-30 15:03:43 +1000

    updating daily weather function

--- src/dayWeather.js
+++ src/dayWeather.js
@@ -2,22 +2,22 @@ import Weather from "./findWeather";
 import {convertToCelcius} from "./findWeather";
 
 function DayWeather(props) {
-    console.log(props.day, 'day')
-
    
-    return (
-        
-        <div id='day-div'>
-        <h4>{props.city}</h4>
-        <p>{props.day[0] ? props.day[0].weather[0].description : 'not loaded'}</p>
-        <p>{props.day[0] ? convertToCelcius(props.day[0].main.temp_min) + ' min temp' : 'not loaded'}</p>
-        <p>{props.day[0] ? convertToCelcius(props.day[0].main.temp_max) + ' max temp': 'not loaded'}</p>
-        </div>
-        );
-
-
+    
+    return props.day.map(hour => {
+        return (
+            
+            <div id='day-div'>
+            <h4>{props.city}</h4> 
+            <p>{hour ? hour.weather.description : 'not loaded'}</p>
+            <p>{hour ? convertToCelcius(hour.main.temp_min) + ' min temp' : 'not loaded'}</p>
+            <p>{hour ? convertToCelcius(hour.main.temp_max) + ' max temp': 'not loaded'}</p>
+          
+            </div>
+            
+            );
+        })
 }
-
 
 
 export default DayWeather
--- src/weekWeather.js
+++ src/weekWeather.js
@@ -1,30 +1,40 @@
-import Weather from "./findWeather";
+
 import {convertToCelcius} from "./findWeather";
 
 function WeekWeather(props) {
-    
+  let currentDay = props.week[0]
+  let currentIndex = 0
+  let daysOfWeek = [[], [], [], [], []]
+  for (let day of props.week){
   
-   
-  console.log(props.week, 'week') 
+   if (currentDay.dt_txt.slice(0,10) === day.dt_txt.slice(0,10)){
+    daysOfWeek[currentIndex].push(day.main.temp)
+   } else {
+    currentIndex ++ 
+    currentDay = day
+    daysOfWeek[currentIndex].push(day.main.temp)
+   }
+  }
+  console.log(daysOfWeek, 'days of week')
+  // console.log(props.week, 'week') 
   return (
     <div>
-        {props.week.map((day, index) => {
-          // console.log(day)
+        {/* {props.week.map((day, index) => {
+         
+ 
             return (
                 <div id='week-div'>
                     <h4>{props.city}</h4>
                     <p>{day.weather[0].description}</p>
-                    <br></br>
                     <p>{convertToCelcius(day.main.temp_max)} max temp</p>
-                    <br></br>
+                    <p>{day.dt_txt}</p>
                     <p>{convertToCelcius(day.main.temp_min)} min temp</p>
                 </div>
             )
-            })}
+            })} */}
        
     </div>
   );
-  
-}
+  }
 
 export default WeekWeather;
